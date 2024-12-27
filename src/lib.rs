2e80d2d0f3c3e6b0e3f56c1eca6b58ea6380fdc4
syvb <me@iter.ca>
2024-12-27 12:56:31 -0500

    use an enum-based implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,50 +1,78 @@
-use std::{any::Any, fmt::Debug};
-
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Expr {
-    pub kind: Box<dyn ExprKind>,
+    pub kind: Box<ExprKind>,
 }
-pub trait ExprKind: PartialEq<Expr> + Debug + Any {
-    fn simplify(&mut self) {}
-}
-
-pub mod ops {
-    use super::*;
-
-    macro_rules! expr_op_impl {
-        ($op:tt, $f:ident) => {
-            #[derive(Debug)]
-            struct $op(Expr, Expr);
-            impl ExprKind for $op {}
-            impl PartialEq<Expr> for $op {
-                fn eq(&self, rhs: &Expr) -> bool {
-                    if let Some(other) = Box::<(dyn Any + 'static)>::downcast(rhs.kind) {
-                        self.0 == other.0 && self.1 == other.1
-                    } else {
-                        false
-                    }
-                }
-            }
-
-            impl std::ops::$op for Expr {
-                type Output = Self;
-                fn $f(self, rhs: Self) -> Self::Output {
-                    Expr {
-                        kind: Box::new($op(self, rhs)),
-                    }
-                }
-            }
-        };
+impl Expr {
+    pub fn i(n: i64) -> Self {
+        Expr {
+            kind: Box::new(ExprKind::Integer(n)),
+        }
     }
-    expr_op_impl!(Add, add);
-    expr_op_impl!(Sub, sub);
-    expr_op_impl!(Div, div);
-    expr_op_impl!(Mul, mul);
 }
 
+#[derive(Debug, Clone)]
+pub enum ExprKind {
+    Integer(i64),
+
+    // simple unary ops
+    Neg(Expr),
+
+    // simple binary ops
+    Add(Expr, Expr),
+    Sub(Expr, Expr),
+    Div(Expr, Expr),
+    Mul(Expr, Expr),
+}
+
+impl Expr {
+    pub fn simplify(self) -> Self {
+        macro_rules! ops {
+            ($expr:expr , $($name:ident : $op:expr,)+) => {
+                match *self.kind {
+                    // already simple
+                    ExprKind::Integer(_) | ExprKind::Div(_, _) => self,
+
+                    // unary
+                    ExprKind::Neg(inner) => {
+                        let inner = inner.simplify();
+                        if let ExprKind::Integer(n) = *inner.kind {
+                            Expr {
+                                kind: Box::new(ExprKind::Integer(-n)),
+                            }
+                        } else {
+                            inner
+                        }
+                    }
+
+
+                    $(
+                        ExprKind::$name(x, y) => {
+                            let x = x.simplify();
+                            let y = y.simplify();
+                            if let (ExprKind::Integer(x), ExprKind::Integer(y)) = (&*x.kind, &*y.kind) {
+                                Expr {
+                                    kind: Box::new(ExprKind::Integer(($op)(x,y))),
+                                }
+                            } else {
+                                Expr {
+                                    kind: Box::new(ExprKind::$name(x, y)),
+                                }
+                            }
+                        }
+                    )+
+                }
+            };
+        }
+        ops!(
+            *self.kind,
+            Add: |x, y| x + y,
+            Sub: |x, y| x - y,
+            Mul: |x, y| x * y,
+        )
+    }
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
 }
